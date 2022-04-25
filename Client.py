501a47204d0e7a04ebfaf5b3bff340bd3c8ed0e3
Noft1337 <maudri324@gmail.com>
2022-04-25 17:17:32 +0300

    Client finished!

--- Client.py
+++ Client.py
@@ -2,6 +2,12 @@ import socket
 import main
 from Variables import *
 
+HEADER = 2048
+TEAM_SET = False
+TEAM_PLAYING = "Playing team: %s"
+OPPOSITE_TEAM = {"White": "Black", "Black": "White"}
+CHECK_MATE = "Checkmate!"
+
 
 def handle_input():
     user_input = ""
@@ -55,18 +61,57 @@ def connection_input():
     return ip, int(port)
 
 
+def set_team(msg: str):
+    if "White" in msg:
+        team = "White"
+    else:
+        team = "Black"
+    TEAM_SET = True
+    return team
+
+
+def validate_response(resp: str, team: str):
+    return TEAM_PLAYING % OPPOSITE_TEAM[team] in resp
+
+
+def send_move(s: socket.socket, team: str):
+    valid = False
+    while not valid:
+        move = input("Move: ")
+        s.send(move.encode())
+        resp = s.recv(HEADER).decode()
+        print(resp)
+        valid = validate_response(resp, team)
+
+
+def check_if_mate(msg: str):
+    return CHECK_MATE in msg
+
+
+def handle_msg(msg: str, team: str, s: socket.socket):
+    print(msg)
+    if TEAM_PLAYING % team in msg:
+        send_move(s, team)
+    return check_if_mate(msg)
+
+
 def client_main():
     online = handle_input()
+    team = ""
     if online == '1':
         main.main()
     else:
         ip, port = connection_input()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, port))
-        while True:
-            msg = s.recv(1024)
+        print(f"Connected to {ip}:{port} successfully!")
+        mate = False
+        while not mate:
+            msg = s.recv(HEADER).decode()
             if msg:
-                print(msg)
+                if not TEAM_SET:
+                    team = set_team(msg)
+                mate = handle_msg(msg, team, s)
 
 
 if __name__ == '__main__':
